57a59ca2476e419e5da4a7443fa6cf24e37b0566
Uma-2507 <152407992+Uma-2507@users.noreply.github.com>
2024-03-20 19:32:05 +0530

    added new file to greetings

---
+++ greetings.java
@@ -0,0 +1,5 @@
+public class greetings {
+    public static void main(String[] args) {
+        System.out.println("Welcome");
+    }
+}
---
+++ uma3.java
@@ -0,0 +1,9 @@
+public class uma3 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 10;
+        int c = a + b;
+        System.out.println(c);
+        System.out.println("uma3");
+    }
+}
